ad46c680d9e49c6dfa66988819fb3287f007021e
Nick Amoscato <nicholas@amoscato.com>
2016-04-23 15:05:35 -0400

    Remove unnecessary distribution template whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var addStream = require('add-stream'),
     gulpConcat = require('gulp-concat'),
     gulpCssnano = require('gulp-cssnano'),
     gulpHelp = require('gulp-help')(gulp),
+    gulpHtmlmin = require('gulp-htmlmin'),
     gulpJshint = require('gulp-jshint'),
     gulpNgAnnotate = require('gulp-ng-annotate'),
     gulpRename = require('gulp-rename'),
@@ -81,13 +82,7 @@ gulp.task('css:dist', 'Compile multiselect SASS', function() {
 });
 
 gulp.task('js:app', 'Compile application JavaScript', function() {
-    var stream = gulp.src(js.src.app)
-        .pipe(addStream.obj(gulp.src(js.src.templates)
-            .pipe(gulpAngularTemplateCache('templates.js', {
-                module: 'amo.multiselect',
-                root: 'amo/multiselect'
-            })
-        )));
+    var stream = gulp.src(js.src.app).pipe(addTemplateStream());
 
     return compileJavaScript(stream, 'app', js.dest);
 });
@@ -103,13 +98,7 @@ gulp.task('js:dist:compressed', false, ['js:dist:uncompressed'], function() {
 });
 
 gulp.task('js:dist:uncompressed', false, function() {
-    var stream = gulp.src(js.src.dist)
-        .pipe(addStream.obj(gulp.src(js.src.templates)
-            .pipe(gulpAngularTemplateCache('templates.js', {
-                module: 'amo.multiselect',
-                root: 'multiselect'
-            })
-        )));
+    var stream = gulp.src(js.src.dist).pipe(addTemplateStream());
 
     return compileJavaScript(stream, 'multiselect', distDest.js, false);
 });
@@ -156,12 +145,12 @@ gulp.task('watch', 'Watch for changes and recompile', ['all'], function() {
 });
 
 /**
- * @ngdoc method
  * @name compileJavaScript
  * @param {Object} stream Stream object
  * @param {String} name Name of output file
  * @param {String} dest Destination path
  * @param {Boolean} [uglify=true]
+ * @returns {Stream}
  */
 function compileJavaScript(stream, name, dest, uglify) {
     if (typeof uglify === 'undefined') {
@@ -182,4 +171,22 @@ function compileJavaScript(stream, name, dest, uglify) {
     }
 
     return stream.pipe(gulp.dest(dest));
+}
+
+/**
+ * @name getTemplateStream
+ * @returns {Stream}
+ */
+function addTemplateStream() {
+    return addStream.obj(
+        gulp.src(js.src.templates)
+            .pipe(gulpHtmlmin({
+                collapseWhitespace: true,
+                conservativeCollapse: true
+            }))
+            .pipe(gulpAngularTemplateCache('templates.js', {
+                module: 'amo.multiselect',
+                root: 'amo/multiselect'
+            }))
+    );
 }
